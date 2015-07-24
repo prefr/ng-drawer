aac159465929946b37550a89764a8653dbbcf5a4
Andreas Pittrich <andreas.pittrich@web.de>
2015-07-24 04:40:12 +0200

    zs

---
+++ demo/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var app  	=   angular.module(
+                            'demo',
+                             [
+                             	'ng',
+                             	'ngDrawer'
+                             ]
+                )
--- src/ng-drawer.js
+++ src/ng-drawer.js
@@ -1,22 +1,86 @@
 angular.module('ngDrawer', [])
 
-
 .directive('ngDrawer',[
 
-	function(){
+	'$document',
+
+	function($document){
 		return {			
+
 			transclude:		true,
+			scope:			true,
+			template:		'<div ng-transclude></div>',
 
-			link: function(scope, element, attrs, controller, transclude){
+			link: function(scope, element, attrs, ngChest, transclude){
 
-				var drawable_wrapper 	= element,
+				scope.drawn = false
+
+
+				var content 	= undefined,
+					wrapper		= element.find('div').eq(0).css({'display':	'block'}),
+					container 	= $document.find('body').eq(0)
+
+
+				container.css({'overflow-x':'hidden'})
+
+				function draw(){			
+
+					if(scope.drawn) return null
+
+					var available_space_left 	=  	element.parent()[0].offsetLeft-container[0].offsetLeft,
+						tucked_width			=	element[0].offsetWidth	
+						tucked_height			=	element[0].offsetHeight	
+
+
+					element.css({
+						'width':		available_space_left+tucked_width+'px',
+						'margin-left':	-available_space_left+'px'						
+					})
+
+					wrapper.css({
+						'padding-left':		available_space_left+'px',
+						'padding-right':	available_space_left+tucked_width+'px'
+					})
+
+					element
+					.addClass('drawn')
+
+					scope.drawn = true
+					
+				}
+
+				function tuck(){
+				 	scope.drawn = false
+				 	element.removeClass('drawn')
+
+				 	element.css({
+				 		'width':			'',
+				 		'margin-left':		''
+				 	})
+
+				 	wrapper.css({
+				 		'padding-left':		'',
+				 		'padding-right':	''
+				 	})
+					
+				}
+
+				element.on('mousedown touchstart', draw)
+
+				$document.find('body').eq(0).on('mouseup touchend touchcancel', tuck)
+
+				scope.$on('$destroy', function(){
+					$document.find('body').eq(0).off('mouseup touchend touchcancel', tuck)
+				})
+
+/*
+
+				
 					drawable_body 		= angular.element('<div></div>')
 
 				element.append(drawable_body)							
 
-				transclude(function(clone){
-					drawable_body.append(clone)
-				})
+
 
 				var	wrapper_tucked	=	{
 											'position':			'absolute',
@@ -53,29 +117,8 @@ angular.module('ngDrawer', [])
 				drawable_body.css(body_tucked)
 
 
-				function draw(){					
-					scope.drawn 	= true
-					scope.tucked 	= false
+				
 
-					drawable_wrapper
-					.css(wrapper_drawn)
-
-					drawable_body
-					.css(body_drawn)
-
-					element.get(0).scrollLeft = 0
-				}
-
-				function tuck(){
-					scope.drawn 	= false
-					scope.tucked 	= true
-
-					drawable_wrapper
-					.css(wrapper_tucked)
-
-					drawable_body
-					.css(body_tucked)
-				}
 
 
 				element.on('mousedown', draw)
@@ -86,6 +129,8 @@ angular.module('ngDrawer', [])
 				scope.$on('$destroy', function(){
 					angular.element('body').off('mouseup', tuck)
 				})
+
+			*/
 			}
 
 		}
